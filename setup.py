83af6378f73798b22db20b82b2a724dcf1725eb0
PiouPiou974 <129684995+PiouPiou974@users.noreply.github.com>
2023-11-12 08:58:58 +0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='basicfoncier',
+    packages=['basicfoncier'],
+    description='Package public de fonctions pour traiter les données foncières (données cadastrales et autres)',
+    version='0.1',
+    url='http://github.com/PiouPiou974/basicfoncier',
+    author='Antoine PETIT',
+    author_email='antoine.petit@lilo.org',
+    keywords=['pip', 'basicfoncier', 'foncier']
+)
