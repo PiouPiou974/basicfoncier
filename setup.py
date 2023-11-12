a842547fb592d4b36935303b21861f0d7f2bf59c
Antoine PETIT <129684995+PiouPiou974@users.noreply.github.com>
2023-11-12 14:37:30 +0400

    Update setup.py
    Added subfolder utils to the package list

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='basicfoncier',
-    packages=['basicfoncier'],
+    packages=['basicfoncier', 'basicfoncier.utils'],
     description='Package public de fonctions pour traiter les données foncières (données cadastrales et autres)',
     version='0.1',
     url='http://github.com/PiouPiou974/basicfoncier',
